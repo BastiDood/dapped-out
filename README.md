3861c2e550a376ce2aa53181857cb4a133e47bbd
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-04-21 21:05:44 +0800

    docs: add home page explainer

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 _Dapped Out!_ is a timing-based casino game for the Solana blockchain. The objective of the game is simple: try to press a button as close to a targeted marker as possible. The closest contestant to the target wins the greatest share in the contested pot.
 
-_Dapped Out!_ accepts any valid token in the Solana blockchain as a currency for each contest. When creating a new contest, simply point it to the mint account of the token being transacted. For convenience, it is also possible to create (only) one brand new program-derived user-generated token instead. The creator has the privilege of minting new supply as well as transferring tokens to other wallets.
+_Dapped Out!_ accepts any valid token in the Solana blockchain as a currency for each contest. When creating a new contest, simply point it to the mint account of the token being transacted. For instance, the `USDC` coin may be transacted by pointing the contest to the mint address `EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v`. For convenience, it is also possible to create (only) one brand new program-derived user-generated token instead. The creator has the privilege of minting new supply as well as transferring tokens to other wallets.
 
-Users may now join the existing contest by staking some tokens into the shared pot. As mentioned earlier, they now try their luck in timing the meter as closely as possible. At any point in the future, the contest host may close the contest to finally distribute the winnings in proportion to the accuracy of each contestant.
+Users may now join the existing contest by staking some tokens into the shared pot. As mentioned earlier, they now try their luck in timing the meter as closely as possible. At any point in the future, the contest host may close the contest to finally distribute the winnings in proportion to the accuracy of each contestant. Past contests are archived for future audits.
 
 ## Development
 
@@ -14,7 +14,7 @@ Users may now join the existing contest by staking some tokens into the shared p
 [Corepack]: https://github.com/nodejs/corepack
 [Node.js]: https://nodejs.org/
 
-For the user interface, _Dapped Out!_ uses the [SvelteKit] framwork for full-stack web development with JavaScript. To add a cherry on top, _Dapped Out!_ features an installable [progressive web application][pwa].
+For the user interface, _Dapped Out!_ uses the [SvelteKit] framework for full-stack web development with JavaScript. To add a cherry on top, _Dapped Out!_ features an installable [progressive web application][pwa].
 
 [SvelteKit]: https://kit.svelte.dev/
 [pwa]: https://web.dev/explore/progressive-web-apps
--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,19 +1,35 @@
+<script>
+    import { Icon } from '@steeze-ui/svelte-icon';
+    import { MarkGithub } from '@steeze-ui/octicons';
+</script>
+
 <article class="prose max-w-none dark:prose-invert">
     <h1>Dapped Out!</h1>
+    <a href="https://github.com/BastiDood/dapped-out" target="_blank" class="variant-filled-primary btn">
+        <Icon src={MarkGithub} theme="mini" class="size-6" />
+        <span>BastiDood/dapped-out</span>
+    </a>
     <p>
-        <em>Dapped Out!</em> is a timing-based casino game for the Solana blockchain. The objective of the game is simple:
-        try to press a button as close to a targeted marker as possible. The closest contestant to the target wins the greatest
-        share in the contested pot.
+        <em>Dapped Out!</em> is a timing-based casino game for the Solana blockchain, implemented as an installable
+        <a href="https://web.dev/explore/progressive-web-apps" target="_blank" class="anchor"
+            >progressive web application</a
+        >. The objective of the game is simple: try to press a button as close to a targeted marker as possible. The
+        closest contestant to the target wins the greatest share in the contested pot.
     </p>
     <p>
-        <em>Dapped Out!</em> accepts any valid token in the Solana blockchain as a currency for each contest. When creating
-        a new contest, simply point it to the mint account of the token being transacted. For convenience, it is also possible
-        to create (only) one brand new program-derived user-generated token instead. The creator has the privilege of minting
-        new supply as well as transferring tokens to other wallets.
+        The application accepts any valid token in the Solana blockchain as a currency for contests. When creating a new
+        contest, simply point it to the mint account of the token being transacted. For instance, the <code
+            class="not-prose code">USDC</code
+        >
+        coin may be transacted by pointing the contest to the mint address
+        <code class="not-prose code">EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v</code>. For convenience, it is also
+        possible to create (only) one brand new program-derived user-generated token instead. The creator has the
+        privilege of minting new supply as well as transferring tokens to other wallets.
     </p>
     <p>
         Users may now join the existing contest by staking some tokens into the shared pot. As mentioned earlier, they
         now try their luck in timing the meter as closely as possible. At any point in the future, the contest host may
-        close the contest to finally distribute the winnings in proportion to the accuracy of each contestant.
+        close the contest to finally distribute the winnings in proportion to the accuracy of each contestant. Past
+        contests are archived for future audits.
     </p>
 </article>
